9b6f60fb9cea07845fa580cf996828a6f9e78aa5
Eugene Zolotarev <eugzol@gmail.com>
2023-10-06 16:31:51 +0300

    Rails 7.1 support (#28)
    ---------
    
    Co-authored-by: Cassio Godinho <cassiopgodinho@gmail.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,10 +5,16 @@ Bundler.require(:default)
 require 'active_record'
 require File.dirname(__FILE__) + '/../lib/where_exists'
 
-ActiveRecord::Base.default_timezone = :utc
+# Rails < 7.1
+if ActiveRecord::Base.respond_to?(:default_timezone=)
+  ActiveRecord::Base.default_timezone = :utc
+else
+  ActiveRecord.default_timezone = :utc
+end
+
 ActiveRecord::Base.time_zone_aware_attributes = true
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
-  :database => File.dirname(__FILE__) + "/db/test.db"
+  :database => File.dirname(__FILE__) + '/db/test.db'
 )
