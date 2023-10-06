4815a51aae9aa1dc09a80cb50fb3ec675a7abf43
Eugene Zolotarev <eugzol@gmail.com>
2023-10-06 17:12:59 +0400

    Quotes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ Bundler.require(:default)
 require 'active_record'
 require File.dirname(__FILE__) + '/../lib/where_exists'
 
-if Gem.latest_spec_for('activerecord').version < Gem::Version.new("7.1")
+if Gem.latest_spec_for('activerecord').version < Gem::Version.new('7.1')
   ActiveRecord::Base.default_timezone = :utc
 else
   ActiveRecord.default_timezone = :utc
@@ -15,5 +15,5 @@ ActiveRecord::Base.time_zone_aware_attributes = true
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
-  :database => File.dirname(__FILE__) + "/db/test.db"
+  :database => File.dirname(__FILE__) + '/db/test.db'
 )
